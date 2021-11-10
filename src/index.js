493bad01cf1a9bda350d32e2dd572b0ba14d5d82
ghodrat keshtmand <60603913+ImAWinner1@users.noreply.github.com>
2021-11-10 17:41:55 +0330

    TimeFunction

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+function tick(){
+    const element= (
+        <div>
+            <h1>Hello World ! </h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    
+        </div>
+    );
+    ReactDom.render(
+        element,
+        document.getElementById('root')
+    );
+
+}
+setInterval(tick,1000);
